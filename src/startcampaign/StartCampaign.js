320d34d13e093adcbb0eea509bb502d28ef7898e
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2020-04-01 00:22:06 +0500

    Cause Details Integerating

--- src/startcampaign/StartCampaign.js
+++ src/startcampaign/StartCampaign.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CampaignFor from "./campaignfor/CampaignFor";
+import CauseDetails from "./causedetails/CauseDetails";
 
 export class StartCampaign extends Component {
   render() {
     return (
       <div>
-        <CampaignFor />
+        {/* <CampaignFor /> */}
+        <CauseDetails />
       </div>
     );
   }
---
+++ src/startcampaign/causedetails/CauseDetails.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Dropdown } from "semantic-ui-react";
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+const causeType = [
+  {
+    key: "Education",
+    text: "Education",
+    value: "Education"
+  },
+  {
+    key: "Health",
+    text: "Health",
+    value: "Health"
+  },
+  {
+    key: "Other",
+    text: "Other",
+    value: "Other"
+  }
+];
+
+const CauseDetails = () => {
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Cause Details
+        </Typography>
+        <span className="mt-3">
+          Raising Funds for{" "}
+          <Dropdown inline options={causeType} value={"Education"} />
+          cause
+        </span>
+      </div>
+
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={6}>
+          <Button fullWidth variant="contained" className={classes.submit}>
+            Back
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default CauseDetails;
