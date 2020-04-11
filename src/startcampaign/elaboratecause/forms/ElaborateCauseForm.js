2e3f68641fb9374f217a12e01ac0d9f28de925ba
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2020-04-12 01:34:42 +0500

    File Uploading

--- src/startcampaign/elaboratecause/forms/ElaborateCauseForm.js
+++ src/startcampaign/elaboratecause/forms/ElaborateCauseForm.js
@@ -7,28 +7,31 @@ import Typography from "@material-ui/core/Typography";
 import Zoom from "@material-ui/core/Zoom";
 import { UploaderComponent } from "@syncfusion/ej2-react-inputs";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(-1)
+    marginTop: theme.spacing(-1),
   },
   root: {
     marginTop: theme.spacing(5),
-    padding: theme.spacing(3, 2)
+    padding: theme.spacing(3, 2),
   },
   gender: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   story: {
     marginTop: theme.spacing(3),
     display: "flex",
-    flexDirection: "column"
-  }
+    flexDirection: "column",
+  },
 }));
 
 const ElaborateCauseForm = ({ props }) => {
   const classes = useStyles();
   const { values, errors, touched, handleChange, handleBlur } = props;
+  const path = {
+    saveUrl: "https://us-central1-react-redux-firebase-d53cd.cloudfunctions.net/uploadFile",
+  };
   return (
     <Zoom in={true}>
       <Paper elevation={5} className={classes.root}>
@@ -63,7 +66,7 @@ const ElaborateCauseForm = ({ props }) => {
             </Typography>
           </Grid>
           <Grid item className="mt-4">
-            <UploaderComponent />
+            <UploaderComponent id="previewfileupload" asyncSettings={path} />
           </Grid>
           {/* End Upload Component */}
         </form>
